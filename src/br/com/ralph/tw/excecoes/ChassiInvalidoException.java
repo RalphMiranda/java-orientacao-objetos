8de225cc5b4e41b910f9c31cc04b429af7788173
Ralph Oliveira de Miranda <ralph.miranda@gmail.com>
2025-05-10 12:26:21 -0300

    Aula 5.4 - Criando construtores personalidados

--- src/br/com/ralph/tw/excecoes/ChassiInvalidoException.java
+++ src/br/com/ralph/tw/excecoes/ChassiInvalidoException.java
@@ -1,5 +1,8 @@
 package br.com.ralph.tw.excecoes;
 
 public class ChassiInvalidoException extends Exception {
-
+    
+    public ChassiInvalidoException(String chassi) {
+        super(String.format("O chassi [%s] informado é inválido!", chassi));
+    }
 }
